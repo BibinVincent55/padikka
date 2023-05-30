4a5c183ed78945a30acb13ef4dcd9cd8c5fd25d7
Bibin Vincent <95958451+BibinVincent55@users.noreply.github.com>
2023-05-30 20:07:15 +0530

    Create main.py

---
+++ class/main.py
@@ -0,0 +1,11 @@
+from StudentClass import Student
+
+# create a new student instance
+student1 = Student("Agrah", "India", 22)
+
+# display student details
+student1.display_student_details()
+
+# set and get age
+student1.set_age(23)
+print(student1.get_age())
